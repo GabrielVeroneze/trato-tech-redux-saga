72474f11e0a1e0e310c3c0f9faf0349cdc7aeba4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-16 22:18:22 -0300

    ♻️ refactor: remove listener para carregar múltiplas categorias

--- src/store/middlewares/categorias.ts
+++ src/store/middlewares/categorias.ts
@@ -1,5 +1,5 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit'
-import { adicionarTodasAsCategorias, adicionarUmaCategoria, carregarCategorias, carregarUmaCategoria } from '@/store/reducers/categorias'
+import { adicionarUmaCategoria, carregarUmaCategoria } from '@/store/reducers/categorias'
 import { RootState, AppDispatch } from '@/store'
 import { Categoria } from '@/types/Categoria'
 import categoriasService from '@/services/categorias'
@@ -11,25 +11,6 @@ export const startAppListening = categoriasListener.startListening.withTypes<
     RootState,
     AppDispatch
 >()
-
-startAppListening({
-    actionCreator: carregarCategorias,
-    effect: async (action, { dispatch, fork, unsubscribe }) => {
-        const resposta = await criarTarefa<Categoria[]>({
-            fork,
-            dispatch,
-            action: adicionarTodasAsCategorias,
-            busca: categoriasService.buscar,
-            textoCarregando: 'Carregando categorias',
-            textoSucesso: 'Categorias carregadas com sucesso!',
-            textoErro: 'Erro na busca de categorias',
-        })
-
-        if (resposta.status === 'ok') {
-            unsubscribe()
-        }
-    },
-})
 
 startAppListening({
     actionCreator: carregarUmaCategoria,
