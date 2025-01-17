50ae46f14f540f574c6a3039ccd4167db7ee55ad
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-16 22:24:13 -0300

    ✨ feat: adiciona saga do carrinho à configuração da store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { categoriasListener } from './middlewares/categorias'
 import { itensListener } from './middlewares/itens'
 import { categoriasSaga } from './sagas/categorias'
+import { carrinhoSaga } from './sagas/carrinho'
 import createSagaMiddleware from 'redux-saga'
 import categoriasSlice from './reducers/categorias'
 import itensSlice from './reducers/itens'
@@ -18,12 +20,14 @@ const store = configureStore({
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().prepend(
+            categoriasListener.middleware,
             itensListener.middleware,
             sagaMiddleware
         ),
 })
 
 sagaMiddleware.run(categoriasSaga)
+sagaMiddleware.run(carrinhoSaga)
 
 export default store
 
