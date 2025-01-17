e3e89af10e3422637ca054bb1a3af57eb059926c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-17 17:42:59 -0300

    ✨ feat: cria serviço para busca de usuários por ID

---
+++ src/services/usuarios.ts
@@ -0,0 +1,12 @@
+import { Usuario } from '@/types/Usuario'
+import instance from '@/common/config/api'
+
+const usuariosService = {
+    buscarPorId: async (id: string): Promise<Usuario> => {
+        const resposta = await instance.get<Usuario>(`/usuarios/${id}`)
+
+        return resposta.data
+    },
+}
+
+export default usuariosService
