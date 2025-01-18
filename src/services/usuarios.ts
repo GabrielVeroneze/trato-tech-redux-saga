cdccebe4ee09d6c5e8fad40a8e7a66f146c168da
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-18 20:07:50 -0300

    ♻️ refactor: ajusta tipo do parâmetro `id` no serviço de usuários

--- src/services/usuarios.ts
+++ src/services/usuarios.ts
@@ -2,7 +2,7 @@ import { Usuario } from '@/types/Usuario'
 import instance from '@/common/config/api'
 
 const usuariosService = {
-    buscarPorId: async (id: string): Promise<Usuario> => {
+    buscarPorId: async (id: number): Promise<Usuario> => {
         const resposta = await instance.get<Usuario>(`/usuarios/${id}`)
 
         return resposta.data
