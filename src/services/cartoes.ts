aad64679535eab634cc40c2d795ce4b44bac4321
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-18 20:07:08 -0300

    ♻️ refactor: ajusta tipo do parâmetro `usuarioId` no serviço de cartões

--- src/services/cartoes.ts
+++ src/services/cartoes.ts
@@ -2,7 +2,7 @@ import { Cartao } from '@/types/Cartao'
 import instance from '@/common/config/api'
 
 const cartoesService = {
-    buscarPorIdUsuario: async (usuarioId: string): Promise<Cartao[]> => {
+    buscarPorIdUsuario: async (usuarioId: number): Promise<Cartao[]> => {
         const resposta = await instance.get<Cartao[]>(
             `/cartoes?usuarioId=${usuarioId}`
         )
