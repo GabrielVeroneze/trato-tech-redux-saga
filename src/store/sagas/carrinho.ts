340f9e88a30a93fe19188c06f11dba6650002587
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-18 20:15:45 -0300

    ✨ feat: cria lógica de obtenção de pagamento com dados de usuário, cartões e bandeiras

--- src/store/sagas/carrinho.ts
+++ src/store/sagas/carrinho.ts
@@ -1,8 +1,36 @@
-import { takeLatest } from 'redux-saga/effects'
+import { call, takeLatest } from 'redux-saga/effects'
 import { carregarPagamento } from '@/store/reducers/carrinho'
+import { Usuario } from '@/types/Usuario'
+import { Cartao } from '@/types/Cartao'
+import { Bandeira } from '@/types/Bandeira'
+import usuariosService from '@/services/usuarios'
+import cartoesService from '@/services/cartoes'
+import bandeirasService from '@/services/bandeiras'
+
+const usuarioLogado = 1
 
 function* obterPagamento() {
-    yield console.log('carregando pagamento')
+    try {
+        const usuario: Usuario = yield call(usuariosService.buscarPorId, usuarioLogado)
+
+        const cartoes: Cartao[] = yield call(cartoesService.buscarPorIdUsuario, usuarioLogado)
+
+        const bandeiraIds = cartoes.map(cartao => cartao.bandeiraId)
+
+        const bandeiras: Bandeira[] = yield call(bandeirasService.buscarPorId, bandeiraIds)
+
+        const cartoesComBandeiras = cartoes.map(cartao => {
+            const bandeiraDoCartao = bandeiras.find(bandeira => Number(bandeira.id) === cartao.bandeiraId)
+
+            return {
+                ...cartao,
+                taxa: bandeiraDoCartao?.taxa,
+                bandeira: bandeiraDoCartao?.nome,
+            }
+        })
+
+        console.log({ ...usuario, cartoes: cartoesComBandeiras })
+    } catch (error) {}
 }
 
 export function* carrinhoSaga() {
