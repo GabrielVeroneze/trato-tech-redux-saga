afd09445c9664a41895ab8f2733a36df1fc7794b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-19 18:21:47 -0300

    ✨ feat: integra informações de usuário e total no componente de pagamento

--- src/pages/Pagamento/index.tsx
+++ src/pages/Pagamento/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { carregarPagamento } from '@/store/reducers/carrinho'
+import { RootState } from '@/store'
 import Header from '@/components/Header'
 import Select from '@/components/Select'
 import Button from '@/components/Button'
@@ -8,6 +9,9 @@ import styles from './Pagamento.module.scss'
 
 const Pagamento = () => {
     const dispatch = useDispatch()
+
+    const usuario = useSelector((state: RootState) => state.usuario)
+    const total = useSelector((state: RootState) => state.carrinho.total)
 
     useEffect(() => {
         dispatch(carregarPagamento())
@@ -18,13 +22,18 @@ const Pagamento = () => {
             <Header titulo="Pagamento" />
             <div className={styles.dados}>
                 <p className={styles.forma}>
-                    Olá usuário! Escolha a forma de pagamento:
+                    Olá {usuario.nome}! Escolha a forma de pagamento:
                 </p>
                 <Select aria-label="Forma de pagamento">
                     <option value="-">Forma de pagamento</option>
+                    {usuario.cartoes.map(cartao => (
+                        <option key={cartao.id} value={cartao.id}>
+                            {cartao.nome}
+                        </option>
+                    ))}
                 </Select>
                 <div className={styles.content}>
-                    <p>Total com taxas: R$ 0.00</p>
+                    <p>Total com taxas: R$ {total.toFixed(2)}</p>
                 </div>
                 <div className={styles.finalizar}>
                     <Button>Finalizar Compra</Button>
