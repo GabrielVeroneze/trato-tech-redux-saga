24e57637aa19169ecf3ff9d7de73aa79839d9560
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-20 22:31:53 -0300

    ✨ feat: adiciona suporte à propriedade `disabled` no componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,16 @@ interface ButtonProps {
     children: React.ReactNode
     type?: 'submit' | 'reset' | 'button'
     onClick?: () => void
+    disabled?: boolean
 }
 
-const Button = ({ children, type = 'button', onClick }: ButtonProps) => {
+const Button = ({ children, type = 'button', onClick, disabled }: ButtonProps) => {
     return (
         <button
             className={styles.button}
             type={type}
             onClick={onClick}
+            disabled={disabled}
         >
             {children}
         </button>
